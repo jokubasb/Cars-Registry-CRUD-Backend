31286329b3e9b60ce07a2650402298f4f3d7a71e
jokubasb <jokubas.beivydas@gmail.com>
2020-03-03 22:25:42 +0200

    retain data

--- src/main/java/com/jokubas/lab1/model/Car.java
+++ src/main/java/com/jokubas/lab1/model/Car.java
@@ -27,7 +27,7 @@ public class Car {
     }
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public long getId() {
         return id;
     }
